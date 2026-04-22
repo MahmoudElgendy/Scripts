d2418b931ac1e8ca2623623414111f7d5ed0f646
Mahmoud Elgendi <mahmoudelgendy1@gmail.com>
2026-04-22 01:34:41 -0500

    Load Exercises.ps1 in MyUtils.psm1

--- MyUtils/MyUtils.psm1
+++ MyUtils/MyUtils.psm1
@@ -1,6 +1,7 @@
 # load function files
 . $PSScriptRoot\Functions\Math.ps1
 . $PSScriptRoot\Functions\Text.ps1
+. $PSScriptRoot\Functions\Exercises.ps1
 
 # Export functions beter move it to .psd1 file
 #Export-ModuleMember -Function Add, Greeting 
